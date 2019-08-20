a7926e85355d3c1e8160dafcca94a69ba5e02773
lelanthran <lelanthran@gmail.com>
2019-08-20 22:29:43 +0200

    WIP: still working on getting the base sqldb_queue to compile.

--- src/sqldb_queue.c
+++ src/sqldb_queue.c
@@ -1,8 +1,29 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "sqldb.h"
 #include "sqldb_queue.h"
 #include "sqldb_queue_query.h"
+
+// Redefining it here because don't want to pollute public namespace with
+// small irrelevant functions.
+static char *lstrdup (const char *src)
+{
+   if (!src)
+      src = "";
+
+   size_t len = strlen (src) + 1;
+
+   char *ret = malloc (len);
+   if (!ret)
+      return NULL;
+
+   return strcpy (ret, src);
+}
+
+
+/* **************************************************************** */
 
 struct sqldb_queue_t {
    char        *name;
@@ -29,13 +50,38 @@ static sqldb_queue_t *sqldb_queue_new (const char *name,
                                        uint64_t msg_expiry,
                                        uint64_t max_length)
 {
-   return NULL;
+   bool error = true;
+   sqldb_queue_t *ret = NULL;
+
+   if (!(ret = malloc (sizeof *ret)))
+      goto errorexit;
+
+   memset (ret, 0, sizeof *ret);
+   ret->name = lstrdup (name);
+   ret->description = lstrdup (description);
+
+   error = false;
+
+errorexit:
+   if (error) {
+      sqldb_queue_del (ret);
+      ret = NULL;
+   }
+
+   return ret;
 }
 
 
+/* **************************************************************** */
+
+int sqldb_queue_create (sqldb_t *db, const char *queue_name)
+{
+   return SQLDB_QUEUE_EEXISTS;
+}
+
 int sqldb_queue_open (sqldb_t *db, const char *queue_name, sqldb_queue_t **dst)
 {
-   return NULL;
+   return SQLDB_QUEUE_EEXISTS;
 }
 
 void sqldb_queue_close (sqldb_queue_t *queue)
